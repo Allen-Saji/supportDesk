910ee57a0a9597a026c4749ebb4a6c0327439541
Allen Saji <allensaji@gmail.com>
2022-10-20 07:31:53 +0530

    add authentication

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,67 +1,101 @@
-import {useState} from 'react'
-import {FaSignInAlt} from 'react-icons/fa'
-import {toast} from 'react-toastify'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    })
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
 
-    const { email, password} = formData
+  const { email, password } = formData
 
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,[e.target.name]: e.target.value,
-        }))
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { isLoading } = useSelector((state) => state.auth)
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  // NOTE: no need for useEffect here as we can catch the
+  // AsyncThunkAction rejection in our onSubmit or redirect them on the
+  // resolution
+  // Side effects shoulld go in event handlers where possible
+  // source: - https://beta.reactjs.org/learn/keeping-components-pure#where-you-can-cause-side-effects
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+    dispatch(login(userData))
+      .unwrap()
+      .then((user) => {
+        // NOTE: by unwrapping the AsyncThunkAction we can navigate the user after
+        // getting a good response from our API or catch the AsyncThunkAction
+        // rejection to show an error message
+        toast.success(`Logged in as ${user.name}`)
+        navigate('/')
+      })
+      .catch(toast.error)
+  }
 
-        
-    }
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
-        <section className="heading">
-        <h1> <FaSignInAlt />Login</h1>
-           <p>Log in to get support </p>
-        </section>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
 
-        <section className="form">
-            <form onSubmit={onSubmit}>
-               
-                <div className="form-group">
-                    <input type="email"
-                    className="form-control" 
-                    id='email'
-                    name='email'
-                    value={email}
-                    onChange={onChange}
-                    placeholder="Enter your email"
-                    required />
-                    
-                </div>
-                <div className="form-group">
-                    <input type="password"
-                    className="form-control" 
-                    id='password'
-                    name='password'
-                    value={password}
-                    onChange={onChange}
-                    placeholder="Set password"
-                    required />
-                   
-                </div>
-               
-                <div className="form-group">
-                    <button className='btn btn-block'>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
     </>
   )
 }
